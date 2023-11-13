2bb9064b04b32bf6bd204910d1ac18cadc861faf
ktkt0305 <ktktsyokugyoukunrn@gmail.com>
2023-11-13 14:29:24 +0900

    第２章　１１月１３日

--- rensyuu2_3.py
+++ rensyuu2_3.py
@@ -1,4 +1,7 @@
-member_hobbies = {
-'a' {'読書','ゲーム','映画','音楽','サッカー'}
+a = {'読書','ゲーム','映画','音楽','サッカー'}
 b = {'野球','サッカー','ゲーム','テニス','ゴルフ'}
-}
+input('心の準備ができたらEnterキーを押してください')
+common = a & b
+total = a | b
+compatibility_rate = len(common)/len(total)*100
+print(f'相性度は{compatibility_rate}%でした')
